ef3ef90ea34edc73e51c68a4a78707cb6b81589f
Rahul Mainwal <mahatmaji360@gmail.com>
2022-11-30 22:48:14 +0530

    vocab app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import "./App.css";
+import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
+import Vocabulary from "./pages/Vocabulary";
+import OneWordSubstitutes from "./pages/OneWordSubstitutes";
+import IdiomPhrases from "./pages/IdiomPhrases";
+import PhrasalVerbs from "./pages/PhrasalVerbs";
+import Antonyms from "./pages/Antonyms";
+import Synonyms from "./pages/Synonyms";
+import AddData from "./pages/AddData";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Search />
+        <Routes path="/">
+          <Route path="/" exact element={<Homepage />} />
+          <Route path="/vocabulary" exact element={<Vocabulary />} />
+          <Route
+            path="/one-word-substitutes"
+            exact
+            element={<OneWordSubstitutes />}
+          />
+          <Route path="/idiom-&-phrases" exact element={<IdiomPhrases />} />
+          <Route path="/phrasal-verbs" exact element={<PhrasalVerbs />} />
+          <Route path="/antonyms" exact element={<Antonyms />} />
+          <Route path="/synonyms" exact element={<Synonyms />} />
+          <Route path="/add-data" exact element={<AddData />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
