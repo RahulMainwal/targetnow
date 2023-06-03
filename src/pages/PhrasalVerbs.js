534b29f88c6bd562fa3562d6f123a8dd8ecf821d
Rahul Mainwal <57298309+RahulMainwal@users.noreply.github.com>
2023-06-03 21:18:18 +0530

    Update PhrasalVerbs.js

--- src/pages/PhrasalVerbs.js
+++ src/pages/PhrasalVerbs.js
@@ -21,17 +21,6 @@ function PhrasalVerbs() {
   const filteredData = gotMainData.filter((x) => {
     return x.titleType === "phrasalVerbs";
   });
-  
-    
-   console.log(filteredData.map((x)=> {
-    return {
-    id: x.id,
-    title: x.title,
-    description: x.description,
-    date: "1-5-2023"
-    }
-  }))
-  
   const [elementId, setElementId] = useState("");
   const [gotMainDataTokenId, setgotMainDataTokenId] = useState([]);
   const [show, setShow] = useState(false);
